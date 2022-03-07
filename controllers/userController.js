2460f4396a6694d16d4ed7a27b68080e4af4fd04
fcogallegos <fgallegosgo@gmail.com>
2022-03-07 09:51:08 -0700

    creating the authentication endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,7 +27,11 @@ const register = async (req, res) => {
     
 };
 
+const authenticate = (req, res) => {
+    
+}
 
 export {
-    register
+    register,
+    authenticate
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { register } from '../controllers/userController.js';
+import { register, authenticate } from '../controllers/userController.js';
 
 
 // Authentication, Register and Users confirmation
 router.post('/', register); //create a new user
+router.post('/login', authenticate);
 
 
+
+ 
 export default router;
