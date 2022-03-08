8d68a4e00a344dfb93a28ff58c74bb008c97de0a
fcogallegos <fgallegosgo@gmail.com>
2022-03-08 13:00:21 -0700

    storing the new password

---
+++ helpers/generateJWT.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+
+
+
+const generateJWT = (id) => {
+    return jwt.sign( { id }, process.env.JWT_SECRET );
+};
+
+
+export default generateJWT;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-import { register, authenticate, confirm, forgetPassword, checkToken } from '../controllers/userController.js';
+import { register, authenticate, confirm,
+forgetPassword, checkToken, newPassword } from '../controllers/userController.js';
 
 
 // Authentication, Register and Users confirmation
@@ -9,6 +10,6 @@ router.post('/', register); //create a new user
 router.post('/login', authenticate);
 router.get('/confirm/:token', confirm);
 router.post('/forget-password', forgetPassword);
-router.get('/forget-password/:token', checkToken);
- 
+router.route("/forget-password/:token").get(checkToken).post(newPassword);
+
 export default router;
