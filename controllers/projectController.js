789b322163369ddfbf78b5f2e332abdd2be7d53a
fcogallegos <fgallegosgo@gmail.com>
2022-03-10 11:32:24 -0700

    creating new projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,9 +1,24 @@
+import Project from "../models/Project.js";
+
+
 
 const getProjects = async (req, res) => {
 
 };
 
-const newProject = async (req, res) => { };
+const newProject = async (req, res) => {
+    
+    const project = new Project(req.body);
+    project.creator = req.user._id;
+
+    try {
+        const projectSaved = await project.save();
+        res.json({ projectSaved });
+    } catch (error) {
+        console.log(error);
+    }
+
+ };
 
 const getProject = async (req, res) => { };
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import conectDB from './config/db.js';
 import userRoutes from "./routes/userRoutes.js";
+import projectRoutes from './routes/projectRoutes.js';
 
 const app = express();
 app.use(express.json());
@@ -15,7 +16,7 @@ conectDB();
 
 //Routing
 app.use("/api/users", userRoutes);
-app.use("/api/projects", userRoutes);
+app.use("/api/projects", projectRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
