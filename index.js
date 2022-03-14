cdc031f7d5a2d3cf6be812c552dcc0f3f02341aa
fcogallegos <fgallegosgo@gmail.com>
2022-03-14 10:15:18 -0700

    routing and functions for the tasks

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from "express";
 
 import dotenv from 'dotenv';
 import conectDB from './config/db.js';
+
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from './routes/projectRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
 
 const app = express();
 app.use(express.json());
@@ -17,6 +19,9 @@ conectDB();
 //Routing
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/taskRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import {
+    addTask,
+    getTask,
+    updateTask,
+    deleteTask,
+    changeStatus
+} from '../controllers/taskController.js';
+import checkAuth from "../middleware/checkAuth.js";
+
+
+const router = express.Router();
+
+router.post('/post', checkAuth, addTask);
+router.route('/:id')
+        .get(checkAuth, getTask)
+        .put(checkAuth,updateTask)
+        .delete(checkAuth, deleteTask);
+
+router.post('/status/:id', checkAuth,changeStatus);        
+
+export default router;
