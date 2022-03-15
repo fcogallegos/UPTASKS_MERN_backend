fab25b6817a346975ce54074b64b67210bba58dc
fcogallegos <fgallegosgo@gmail.com>
2022-03-15 10:08:08 -0700

    update task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -48,7 +48,35 @@ const getTask = async (req, res) => {
    res.json(task);
 };
 
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+    const { id } = req.params;
+   //console.log(id);
+
+   if( !mongoose.Types.ObjectId.isValid(id) ) {
+        const error = new Error('Task not found');
+        return res.status(404).json({ msg: error.message });
+   }
+   
+   const task = await Task.findById(id).populate('project');
+   //console.log(task);
+
+   if( task.project.creator.toString() !== req.user._id.toString() ) {
+        const error = new Error('Action not valid');
+        return res.status(403).json({ msg: error.message }); 
+   }
+
+   task.name = req.body.name || task.name;
+   task.description = req.body.description || task.description;
+   task.priority = req.body.priority || task.priority;
+   task.deliveryDate = req.body.deliveryDate || task.deliveryDate;
+
+   try {
+       const taskSaved = await task.save();
+       res.json(taskSaved);
+   } catch (error) {
+       console.log(error);
+   }
+};
 
 const deleteTask = async (req, res) => {};
 
